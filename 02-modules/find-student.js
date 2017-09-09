2414c4fc51f83153ac44448860d977ad4bd62222
Edy Segura <ednardo@inatel.br>
2017-09-09 00:03:28 -0300

    Solution 2

--- 02-modules/find-student.js
+++ 02-modules/find-student.js
@@ -1,19 +1,22 @@
 const students = require('./students.json')
 const studentName = process.argv[2]
 
-function studentById(id) {
-    return function callback(student) {
-        return student.id == id
-    }
-}
-
 function studentByName(name) {
     return function callback(student) {
-        return student.name.toLowerCase().includes(name.toLowerCase())
+        const regexp = new RegExp(name, 'i')
+        return regexp.test(student.name)
     }
 }
 
-const student = students.find(studentByName(studentName))
-let output = student ? student.name : 'Student not found'
+function printName(student) {
+    console.log(student.name)
+}
 
-console.log(output)
+const studentsFound = students.filter(studentByName(studentName))
+
+if(studentsFound.length > 0) {
+    studentsFound.forEach(printName)
+}
+else {
+    console.log('Student not found!')
+}
