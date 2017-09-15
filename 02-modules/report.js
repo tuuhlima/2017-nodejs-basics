f87071fee1b5608808be42dbdeb92c15b4657688
Marcio Azevedo <mecva.mecva@gmail.com>
2017-09-14 22:20:48 -0300

    Solve exercise

--- 02-modules/report.js
+++ 02-modules/report.js
@@ -2,9 +2,19 @@ const students = require('./students.json')
 
 let maleCount = 0
 let femaleCount = 0
+let repeatedNames = {}
 
 for (var i = 0; i < students.length; i++) {
     var student = students[i];
+    var firstName = student.name.split(" ")[0]
+
+    if(repeatedNames[firstName] != null) {
+        repeatedNames[firstName]++
+    }
+    else {
+        repeatedNames[firstName] = 0
+    }
+
     if(student.genre == 'Female') {
         femaleCount++
     }
@@ -13,5 +23,11 @@ for (var i = 0; i < students.length; i++) {
     }
 }
 
+let studentsWithTheSameName = Object.values(repeatedNames)
+                              .reduce((previousValue, currentValue) => {
+                                return previousValue + currentValue
+                              })
+
 console.log("Women Students: " + femaleCount)
 console.log("Men Students: " + maleCount)
+console.log("Repeated names " + (studentsWithTheSameName * 2))
